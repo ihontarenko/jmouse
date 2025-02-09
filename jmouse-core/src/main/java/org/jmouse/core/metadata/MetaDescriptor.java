91e049d5c24c6d7550c13ac67b464e1ced80b0d0
Mr. Jerry Mouse <ihontarenko@gmail.com>
2025-02-09 22:52:40 +0200

    jMouse - MetaDesciptor API

--- jmouse-core/src/main/java/org/jmouse/core/metadata/MetaDescriptor.java
+++ jmouse-core/src/main/java/org/jmouse/core/metadata/MetaDescriptor.java
@@ -33,25 +33,30 @@ public final class MetaDescriptor {
     private static final int                            DEFAULT_DEPTH = 4;
     private static final Map<Class<?>, ClassDescriptor> CACHE         = new HashMap<>();
 
-    private MetaDescriptor() {
-        // Prevent instantiation
-    }
+    private MetaDescriptor() {}
 
     /**
      * Creates a descriptor for a method parameter.
      *
      * @param parameter the parameter to describe
-     * @return a {@link ParameterDescriptor} instance
+     * @return a {@link ParameterDescriptor} instance representing the parameter
      */
     public static ParameterDescriptor forParameter(Parameter parameter) {
         return forParameter(parameter, DEFAULT_DEPTH);
     }
 
+    /**
+     * Creates a descriptor for a method parameter with a specified depth for nested elements.
+     *
+     * @param parameter the parameter to describe
+     * @param depth     the recursion depth limit for nested metadata resolution
+     * @return a {@link ParameterDescriptor} instance
+     */
     private static ParameterDescriptor forParameter(Parameter parameter, int depth) {
         ParameterDescriptor.Builder builder = new ParameterDescriptor.Builder(parameter.getName());
 
         for (Annotation annotation : parameter.getAnnotations()) {
-            builder.annotation(forAnnotation(annotation));
+            builder.annotation(forAnnotation(annotation, depth - 1));
         }
 
         builder.type(forClass(parameter.getType(), depth - 1));
@@ -64,42 +69,44 @@ public final class MetaDescriptor {
      * Creates a descriptor for a method.
      *
      * @param method the method to describe
-     * @return a {@link MethodDescriptor} instance
+     * @return a {@link MethodDescriptor} instance representing the method
      */
     public static MethodDescriptor forMethod(Method method) {
         return forMethod(method, DEFAULT_DEPTH);
     }
 
+    /**
+     * Creates a descriptor for a method with a specified depth for nested elements.
+     *
+     * @param method the method to describe
+     * @param depth  the recursion depth limit for nested metadata resolution
+     * @return a {@link MethodDescriptor} instance
+     */
     private static MethodDescriptor forMethod(Method method, int depth) {
         MethodDescriptor.Builder builder = new MethodDescriptor.Builder(method.getName());
 
-        builder.internal(method).returnType(forClass(method.getReturnType(), depth - 1));
+        forExecutable(builder, method, depth);
 
-        for (Parameter parameter : method.getParameters()) {
-            builder.parameter(forParameter(parameter, depth - 1));
-        }
-
-        for (Class<?> exceptionType : method.getExceptionTypes()) {
-            builder.exceptionType(forClass(exceptionType, depth - 1));
-        }
-
-        for (Annotation annotation : method.getAnnotations()) {
-            builder.annotation(forAnnotation(annotation, depth - 1));
-        }
-
-        return builder.build();
+        return builder.returnType(forClass(method.getReturnType(), depth - 1)).build();
     }
 
     /**
      * Creates a descriptor for a field.
      *
      * @param field the field to describe
-     * @return a {@link FieldDescriptor} instance
+     * @return a {@link FieldDescriptor} instance representing the field
      */
     public static FieldDescriptor forField(Field field) {
         return forField(field, DEFAULT_DEPTH);
     }
 
+    /**
+     * Creates a descriptor for a field with a specified depth for nested elements.
+     *
+     * @param field the field to describe
+     * @param depth the recursion depth limit for nested metadata resolution
+     * @return a {@link FieldDescriptor} instance
+     */
     private static FieldDescriptor forField(Field field, int depth) {
         FieldDescriptor.Builder builder = new FieldDescriptor.Builder(field.getName());
 
@@ -116,26 +123,45 @@ public final class MetaDescriptor {
      * Creates a descriptor for a constructor.
      *
      * @param constructor the constructor to describe
-     * @return a {@link ConstructorDescriptor} instance
+     * @return a {@link ConstructorDescriptor} instance representing the constructor
      */
     public static ConstructorDescriptor forConstructor(Constructor<?> constructor) {
         return forConstructor(constructor, DEFAULT_DEPTH);
     }
 
+    /**
+     * Creates a descriptor for a constructor with a specified depth for nested elements.
+     *
+     * @param constructor the constructor to describe
+     * @param depth       the recursion depth limit for nested metadata resolution
+     * @return a {@link ConstructorDescriptor} instance
+     */
     private static ConstructorDescriptor forConstructor(Constructor<?> constructor, int depth) {
         ConstructorDescriptor.Builder builder = new ConstructorDescriptor.Builder(constructor.getName());
 
-        builder.internal(constructor);
+        forExecutable(builder, constructor, depth);
 
-        for (Parameter parameter : constructor.getParameters()) {
+        return builder.build();
+    }
+
+    /**
+     * Creates a descriptor for a constructor or method with a specified depth for nested elements.
+     */
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    private static void forExecutable(ExecutableDescriptor.Builder builder, Executable executable, int depth) {
+        builder.internal(executable);
+
+        for (Parameter parameter : executable.getParameters()) {
             builder.parameter(forParameter(parameter, depth - 1));
         }
 
-        for (Class<?> exceptionType : constructor.getExceptionTypes()) {
+        for (Class<?> exceptionType : executable.getExceptionTypes()) {
             builder.exceptionType(forClass(exceptionType, depth - 1));
         }
 
-        return builder.build();
+        for (Annotation annotation : executable.getAnnotations()) {
+            builder.annotation(forAnnotation(annotation, depth - 1));
+        }
     }
 
     /**
@@ -145,19 +171,30 @@ public final class MetaDescriptor {
      * </p>
      *
      * @param type the class to describe
-     * @return a {@link ClassDescriptor} instance
+     * @return a {@link ClassDescriptor} instance representing the class
      */
     public static ClassDescriptor forClass(Class<?> type) {
         return forClass(type, DEFAULT_DEPTH);
     }
 
+    /**
+     * Creates a descriptor for a class with a specified depth for nested elements.
+     * <p>
+     * This method retrieves the class descriptor from an internal cache if available.
+     * It recursively analyzes the class's fields, methods, constructors, and annotations,
+     * depending on the specified depth.
+     * </p>
+     *
+     * @param type  the class to describe
+     * @param depth the recursion depth limit for nested metadata resolution
+     * @return a {@link ClassDescriptor} instance
+     */
     private static ClassDescriptor forClass(Class<?> type, int depth) {
-        ClassDescriptor.Builder builder = new ClassDescriptor.Builder(getShortName(type));
-
         if (CACHE.containsKey(type)) {
             return CACHE.get(type);
         }
 
+        ClassDescriptor.Builder builder = new ClassDescriptor.Builder(getShortName(type));
         builder.internal(type);
 
         if (depth > 0) {
@@ -179,6 +216,7 @@ public final class MetaDescriptor {
         }
 
         ClassDescriptor descriptor = builder.build();
+
         CACHE.put(type, descriptor);
 
         return descriptor;
