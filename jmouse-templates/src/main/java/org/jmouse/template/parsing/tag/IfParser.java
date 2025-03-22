82ff6def55d37bfdc0d5ad66b4ec7d6d00aa73c2
Mr. Jerry Mouse <ihontarenko@gmail.com>
2025-03-22 17:15:07 +0200

    jMouse - Template (Leafs) WIP

--- jmouse-templates/src/main/java/org/jmouse/template/parsing/tag/IfParser.java
+++ jmouse-templates/src/main/java/org/jmouse/template/parsing/tag/IfParser.java
@@ -1,5 +1,7 @@
 package org.jmouse.template.parsing.tag;
 
+import org.jmouse.core.matcher.Matcher;
+import org.jmouse.el.CursorMatcher;
 import org.jmouse.el.lexer.TokenCursor;
 import org.jmouse.el.node.ExpressionNode;
 import org.jmouse.el.node.RenderableNode;
@@ -7,6 +9,8 @@ import org.jmouse.el.parser.*;
 import org.jmouse.template.TemplateToken;
 import org.jmouse.template.node.IfNode;
 import org.jmouse.template.parsing.BodyParser;
+
+import static org.jmouse.template.TemplateToken.*;
 
 public class IfParser implements TagParser {
 
@@ -20,8 +24,12 @@ public class IfParser implements TagParser {
 
         cursor.ensure(TemplateToken.T_CLOSE_EXPRESSION);
 
+        Matcher<TokenCursor> matcher  = CursorMatcher.sequence(T_OPEN_EXPRESSION, T_ELSE);
+        matcher = matcher.or(CursorMatcher.sequence(T_OPEN_EXPRESSION, T_ELSE_IF));
+        matcher = matcher.or(CursorMatcher.sequence(T_OPEN_EXPRESSION, T_END_IF));
+
         while (true) {
-            ParserOptions options = ParserOptions.withStopCondition(c -> c.matchesSequence(TemplateToken.T_ELSE_IF) || c.matchesSequence(TemplateToken.T_OPEN_EXPRESSION, TemplateToken.T_ELSE) || c.matchesSequence(TemplateToken.T_ELSE_IF));
+            ParserOptions options = ParserOptions.withStopCondition(c -> c.matchesSequence(TemplateToken.T_ELSE_IF) || c.matchesSequence(T_OPEN_EXPRESSION, T_ELSE) || c.matchesSequence(TemplateToken.T_ELSE_IF));
             context.setOptions(options);
             context.getParser(BodyParser.class).parse(cursor, context);
             System.out.println("qwerqweqwe");
