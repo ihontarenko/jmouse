a90753d8291f81077b57486aaaaeb31374211fa5
Mr. Jerry Mouse <ihontarenko@gmail.com>
2026-03-11 12:35:17 +0200

    jMouse - Action Executor/Invoker/Annotation

--- jmouse-action/src/main/java/org/jmouse/action/ArrayArgumentsMethodArgumentResolver.java
+++ jmouse-action/src/main/java/org/jmouse/action/ArrayArgumentsMethodArgumentResolver.java
@@ -6,23 +6,47 @@ import org.jmouse.core.invoke.MethodArgumentResolver;
 
 import java.util.List;
 
+/**
+ * {@link MethodArgumentResolver} that resolves arguments from a predefined array/list. 📦
+ *
+ * <p>
+ * Arguments are matched by the parameter index of the invoked method.
+ * This resolver typically acts as a simple positional argument provider.
+ * </p>
+ */
 public class ArrayArgumentsMethodArgumentResolver implements MethodArgumentResolver {
 
     private final List<Object> arguments;
 
+    /**
+     * Creates resolver from vararg arguments.
+     *
+     * @param arguments argument values
+     */
     public ArrayArgumentsMethodArgumentResolver(Object... arguments) {
         this(List.of(arguments));
     }
 
+    /**
+     * Creates resolver from argument list.
+     *
+     * @param arguments argument values
+     */
     public ArrayArgumentsMethodArgumentResolver(List<Object> arguments) {
         this.arguments = arguments;
     }
 
+    /**
+     * Supports all parameters.
+     */
     @Override
     public boolean supports(MethodParameter parameter) {
         return true;
     }
 
+    /**
+     * Returns argument by parameter index.
+     */
     @Override
     public Object resolve(MethodParameter parameter, InvocationRequest request) {
         return arguments.get(parameter.getParameterIndex());
